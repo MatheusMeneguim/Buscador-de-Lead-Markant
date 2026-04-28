739ade33970a77d5ec4a7169e0db48ee9f3230c1
Matheus Meneguim <matheusmeneguim123@gmail.com>
2026-04-27 23:55:04 -0300

    feat: cria TabelaLeads com useMemo para filtragem de resultados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { LeadProvider } from './contexts/LeadContext'
 import FormBusca from './components/FormBusca'
+import TabelaLeads from './components/TabelaLeads'
 
 function App() {
   return (
     <LeadProvider>
       <h1>Buscador de Leads Qualificados</h1>
       <FormBusca />
+      <TabelaLeads />
     </LeadProvider>
   )
 }
